b7d4f422316f5cab00a9f75025879cfa066c06fa
dwilso95 <dwilsojhu95@gmail.com>
2017-10-05 14:11:48 +0000

    Adding lots of implementation

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -3,13 +3,46 @@ import java.io.File;
 public class Driver {
 
     public static void main(String[] args) throws Exception {
-        if(args.length == 0){
+
+        if (args.length == 0) {
             System.out.println("No arguments provided. Please enter a command.");
         }
-        
-        ClassLoader cl = new DirectoryClassLoader(new File(args[0]).toURI().toURL());
-        Object o = cl.loadClass("TestClass");
-        System.out.println(o);        
+
+        final String command = args[0];
+
+        if (command.equals("1")) {
+            System.out.println("----- Homework 2 - Part 1 -----");
+            try (final DirectoryClassLoader cl1 = new DirectoryClassLoader(new File(args[0]).toURI().toURL());
+                final DirectoryClassLoader cl2 = new DirectoryClassLoader(new File(args[0]).toURI().toURL());) {
+                System.out.println("----- Testing Same ClassLoader -----");
+                testClassLoaders(cl1, cl1);
+                System.out.println("----- Testing Different ClassLoader -----");
+                testClassLoaders(cl1, cl2);
+            }
+        }
 
     }
+
+    private static void testClassLoaders(ClassLoader cl1, ClassLoader cl2) throws Exception {
+        final Class< ? > class1 = cl1.loadClass("TestClass");
+        final Class< ? > class2 = cl2.loadClass("TestClass");
+
+        final Object o1 = class1.newInstance();
+        final Object o2 = class2.newInstance();
+
+        //Print hashcodes
+        System.out.println("Hashcode of ClassLoaders and TestClass instances");
+        System.out.println("TestClass [" + class1.hashCode() + "] loaded by classloader DirectoryClassLoader [" + cl1.hashCode() + "]");
+        System.out.println("TestClass [" + class2.hashCode() + "] loaded by classloader DirectoryClassLoader [" + cl2.hashCode() + "]");
+        System.out.println("Object 1 [" + o1.hashCode() + "] an instance of class TestClass [" + class1.hashCode() + "]");
+        System.out.println("Object 2 [" + o2.hashCode() + "] an instance of class TestClass [" + class2.hashCode() + "]");
+
+        System.out.println("Comapring TestClass objects 1 & 2");
+        if (o1.equals(o2)) {
+            System.out.println("Objects are equal");
+        } else {
+            System.out.println("Objects are not equal");
+        }
+    }
+
 }
--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,10 +1,10 @@
 public class TestClass {
 
-	public boolean equals(Object o) {
-		if (o instanceof TestClass)
+	public boolean equals(Object class1) {
+		if (class1 instanceof TestClass)
 			System.out.println("yes, it's one of us!");
 		else
 			System.out.println("no, it's not one of us!");
-		return this == (TestClass)o;
+		return this == (TestClass)class1;
 	}
 }
